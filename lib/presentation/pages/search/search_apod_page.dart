a738392b9c427c4dc80216c3b90bfd586012818a
AmandaFonseca <amandafonseca207@gmail.com>
2026-03-24 00:54:38 -0300

    feature: create search

--- lib/presentation/pages/search/search_apod_page.dart
+++ lib/presentation/pages/search/search_apod_page.dart
@@ -4,10 +4,12 @@ import 'package:astronomy_picture/container_injection.dart';
 import 'package:astronomy_picture/custom_colors.dart';
 import 'package:astronomy_picture/domain/entities/apod.dart';
 import 'package:astronomy_picture/presentation/bloc/search/search_bloc.dart';
+import 'package:astronomy_picture/presentation/core/date_convert.dart';
 import 'package:astronomy_picture/presentation/pages/today_apod/apod_view_page.dart';
 import 'package:astronomy_picture/presentation/widgets/core/apod_tile.dart';
 import 'package:astronomy_picture/presentation/widgets/today_apod/error_apod_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class SearchApodPage extends SearchDelegate {
   late SearchBloc _searchBloc;
@@ -17,10 +19,10 @@ class SearchApodPage extends SearchDelegate {
 
   List<Apod> _cacheApodList = [];
   List<String> _searchHistoryList = [];
-  // PickerDateRange _choosedDate = PickerDateRange(
-  //   DateTime.now(),
-  //   (DateTime.now()),
-  // );
+  PickerDateRange _choosedDate = PickerDateRange(
+    DateTime.now(),
+    (DateTime.now()),
+  );
 
   SearchApodPage() {
     _searchBloc = getIt<SearchBloc>();
@@ -39,7 +41,51 @@ class SearchApodPage extends SearchDelegate {
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      IconButton(onPressed: () {}, icon: Icon(Icons.calendar_month)),
+      IconButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) => SfDateRangePicker(
+              showActionButtons: true,
+              maxDate: DateTime.now(),
+              initialSelectedRange: _choosedDate,
+              selectionMode: DateRangePickerSelectionMode.range,
+              backgroundColor: CustomColors.black,
+              todayHighlightColor: CustomColors.blue,
+              headerHeight: 100,
+              headerStyle: DateRangePickerHeaderStyle(
+                textStyle: TextStyle(color: CustomColors.white),
+              ),
+              monthViewSettings: DateRangePickerMonthViewSettings(
+                viewHeaderStyle: DateRangePickerViewHeaderStyle(
+                  textStyle: TextStyle(color: CustomColors.white),
+                ),
+              ),
+              monthCellStyle: DateRangePickerMonthCellStyle(
+                textStyle: TextStyle(color: CustomColors.white),
+              ),
+              onSubmit: (dateRange) {
+                if (dateRange is PickerDateRange) {
+                  _choosedDate = dateRange;
+                  if (dateRange.endDate != null) {
+                    query =
+                        "${DateConvert.dateToString(dateRange.startDate ?? DateTime(2023))}/${DateConvert.dateToString(dateRange.endDate ?? DateTime(2023))}";
+                  } else {
+                    query = DateConvert.dateToString(
+                      dateRange.startDate ?? DateTime(2023),
+                    );
+                  }
+                }
+                Navigator.pop(context);
+              },
+              onCancel: () {
+                Navigator.pop(context);
+              },
+            ),
+          );
+        },
+        icon: const Icon(Icons.calendar_month),
+      ),
       IconButton(
         onPressed: () {
           if (query.isEmpty) {
@@ -48,7 +94,7 @@ class SearchApodPage extends SearchDelegate {
             query = "";
           }
         },
-        icon: Icon(Icons.close),
+        icon: const Icon(Icons.close),
       ),
     ];
   }
@@ -149,6 +195,63 @@ class SearchApodPage extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container();
+    return Container(
+      color: CustomColors.spaceBlue,
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsetsGeometry.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: CustomColors.white.withOpacity(.7)),
+              ),
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                width: double.infinity,
+                child: Center(
+                  child: Text(
+                    "Single day: YYYY-MM-DD\nRange of days: YYYY-MM-DD/YYYY-MM-DD\nOr tap the calendar icon! Is much better",
+                    style: TextStyle(color: CustomColors.white),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          StreamBuilder(
+            stream: _stream.stream,
+            builder: (context, snapshot) {
+              SearchState? state = snapshot.data;
+              if (state is LoadingSearchState) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (state is SuccessHistorySearchState) {
+                _searchHistoryList = state.list;
+              }
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: _searchHistoryList.length,
+                  itemBuilder: (context, index) => ListTile(
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete, color: CustomColors.white),
+                      onPressed: () {
+                        _searchHistoryList.removeAt(index);
+                        _searchBloc.input.add(
+                          UpdateHistorySearchEvent(list: _searchHistoryList),
+                        );
+                      },
+                    ),
+                    title: Text(
+                      _searchHistoryList[index],
+                      style: TextStyle(color: CustomColors.white),
+                    ),
+                    onTap: () => query = _searchHistoryList[index],
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
